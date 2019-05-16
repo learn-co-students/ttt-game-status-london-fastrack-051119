8445edd78766312dfdc3991a7cfc9fb59db004e4
Eliot Howes <ejhowes@gmail.com>
2019-05-16 17:34:46 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,14 +15,49 @@ WIN_COMBINATIONS = [
   [6, 4, 2]
 ]
 
-def won(board)
-  WIN_COMBINATIONS.select do |winning_combination|
-    winning_combination.each do |winning_positions|
-      if winning_positions == ""
-        return true
-      end
-      return false
+
+def won?(board)
+  # if board.all? {|board_positions| board_positions == " "}
+  #   false
+  # end
+  # elsif board.select.with_index {|board_positions, index|
+  #     board_positions == "X" or "O" && index == WIN_COMBINATIONS.select {|winning_combination|
+  #     winning_combination.select {|winning_position| winning_position}}}
+  #     return WIN_COMBINATIONS[0]
+  WIN_COMBINATIONS.each do |winning_positions|
+    if winning_positions.all? {|game_square| board[game_square] == "X"} || winning_positions.all? {|game_square| board[game_square] == "O"}
+      return winning_positions
     end
   end
+  return false
+end
 
+def full?(board)
+  board.none? {|game_square| game_square == " "}
+end
+
+def draw?(board)
+  if full?(board) && won?(board)
+    false
+  elsif !full?(board) && !won?(board)
+    false
+  elsif full?(board) && !won?(board)
+    true
+  end
+end
+
+def over?(board)
+  if full?(board) || draw?(board) || won?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if over?(board) == true && won?(board) == false
+     nil
+  elsif won?(board)[0] && won?(board)[1] && won?(board)[2] == "X"
+     return "X"
+  elsif won?(board)[0] && won?(board)[1] && won?(board)[2] == "O"
+    return "O"
+  end
 end
