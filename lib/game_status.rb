3ad066fa078c2dc991b22290f7fdb9be7792628b
Eliot Howes <ejhowes@gmail.com>
2019-05-16 21:21:26 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,17 +46,11 @@ def over?(board)
 end
 
 def winner(board)
-  # if draw?(board) == true
-  #   return nil
-  # end
-position_1 = won?(board)[0]
-position_2 = won?(board)[1]
-position_3 = won?(board)[2]
-if board[position_1] && board[position_2] && board[position_3] == "X"
-  return "X"
-end
-
-  # if over?(board) && (won?(board)[0] && won?(board)[1] && won?(board)[2] == "X")
-  #   return "X"
-  # end
+  if !won?(board)
+      return nil
+  elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "X"
+      return "X"
+  elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "O"
+      return "O"
+  end
 end
